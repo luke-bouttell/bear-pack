7ac6c9ba9ac083aa2e70c4cdf89ca8898c196330
luke-bouttell <lukeb@defyhi.com>
2022-12-23 10:44:28 +1100

    working prototype

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,9 @@ RUN apt-get update && apt-get install -y \
     software-properties-common
 RUN add-apt-repository universe
 RUN apt-get update && apt-get install -y \ 
-    python3-pip
+    python3-pip \
+    libusb-1.0-0-dev
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-COPY . .
+COPY rs_capture.py .
 CMD python3 rs_capture.py
--- rs_capture.py
+++ rs_capture.py
@@ -7,11 +7,14 @@ config = rs.config()
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 30)
 config.enable_record_to_file('test.bag')
-pipeline.start(config)
-try:
-    start = time.time()
-    while time.time()-start < 20:
-        pipeline.wait_for_frames().keep()
+num = 0
+while True:
+    try:
+        config.enable_record_to_file('rs_'+str(round(time.time()))+'.bag')
+        pipeline.start(config)
+        start = time.time()
+        while time.time()-start < 20:
+            pipeline.wait_for_frames().keep()
 
-finally:
-    pipeline.stop()
+    finally:
+        pipeline.stop()
