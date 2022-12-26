62c44c97b1871c2447123d849c3d3a63ebbc605a
luke-bouttell <lukeb@defyhi.com>
2022-12-27 00:01:10 +1100

    data roll over added, multiprocessing added

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,10 @@ RUN apt-get update && apt-get install -y \
     libusb-1.0-0-dev
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
+
+COPY rosbag_rollover.py .
 COPY rs_capture.py .
-CMD ["python3", "-u", "rs_capture.py"]
+COPY process_handler.py .
+COPY graceful_killer.py .
+
+ENTRYPOINT ["python3", "-u", "process_handler.py"]
--- rs_capture.py
+++ rs_capture.py
@@ -1,38 +1,33 @@
 # First import the library
 import pyrealsense2 as rs
 import time
-import signal
+from graceful_killer import GracefulKiller
 
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 30)
-config.enable_stream(rs.stream.accel)
-config.enable_stream(rs.stream.gyro)
-config.enable_record_to_file('test.bag')
+def rs_capture():
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 30)
+    config.enable_stream(rs.stream.accel)
+    config.enable_stream(rs.stream.gyro)
 
-class GracefulKiller:
-    kill_now = False
-    def __init__(self):
-        signal.signal(signal.SIGINT, self.exit_gracefully)
-        signal.signal(signal.SIGTERM, self.exit_gracefully)
-    
-    def exit_gracefully(self, *args):
-        self.kill_now = True
+    killer = GracefulKiller()
 
-video_length = 30 #video length in seconds
-killer = GracefulKiller()
-while True:
-    config.enable_record_to_file('rs_'+str(round(time.time()))+'.bag') #Writes epoch into file name
-    pipeline.start(config)
-    start = time.time()
-    while time.time()-start < video_length:
-        pipeline.wait_for_frames().keep()
-        if killer.kill_now: #Gracefully ends script on shutdown to prevent file corruption
-            print("Stopping gracefully")
-            pipeline.stop()
-            time.sleep(7)
-            quit()
-    pipeline.stop()
-        
+    video_length = 30 #video length in seconds
+    while True:
+        config.enable_record_to_file('rs_'+str(round(time.time()))+'.bag') #Writes epoch into file name
+        pipeline.start(config)
+        start = time.time()
+        while time.time()-start < video_length:
+            pipeline.wait_for_frames().keep()
+            if killer.kill_now: #Gracefully ends script on shutdown to prevent file corruption
+                print("rs_capture: stopping gracefully")
+                pipeline.stop()
+                time.sleep(10)
+                print("rs_capture: stopped gracefully")
+                quit()
+        pipeline.stop()
+        print("File recorded")
 
+if __name__ == '__main__':
+    rs_capture()
